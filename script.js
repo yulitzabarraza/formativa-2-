4e0420af29b47b59cb63f2e2a8a9c78752758deb
yulitzabarraza <79957126+yulitzabarraza@users.noreply.github.com>
2021-03-14 22:30:42 -0500

    Add files via upload

---
+++ script.js
@@ -0,0 +1,36 @@
+// Activador de jQuery - Función principal de jQuery
+$(document).ready(function(){
+
+    // Activación de transición del menú principal y del menú móvil
+    $('header article nav a, aside#menu-moviles nav a').bind('click',function(event){
+        var $anchor = $(this);
+        $('html, body').stop().animate({
+            scrollTop: $($anchor.attr('href')).offset().top
+        }, 1000,'easeOutExpo');
+        event.preventDefault();
+    });
+
+    // Funciones de Abrir y Cerrar el Menú Responsive
+    $('a#abrir-menu').click(function(){
+        $('aside#menu-moviles').animate({
+            right: 0
+        },500,'easeOutExpo');
+
+        $('a#abrir-menu').hide();
+        $('a#cerrar-menu').show();
+
+        event.preventDefault();
+    });
+
+    $('a#cerrar-menu, aside#menu-moviles nav a').click(function(){
+        $('aside#menu-moviles').animate({
+            right: -290
+        },1000,'easeOutElastic');
+        
+        $('a#abrir-menu').show();
+        $('a#cerrar-menu').hide();
+
+        event.preventDefault();
+    });
+    
+});
